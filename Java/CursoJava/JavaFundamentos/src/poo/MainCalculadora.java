b1390f3bfa35be80b2fd153474815283f0642a71
Daniel_Ramos <danielramosoo36@gmail.com>
2026-06-15 16:32:58 -0500

    guardar

--- Java/CursoJava/JavaFundamentos/src/poo/MainCalculadora.java
+++ Java/CursoJava/JavaFundamentos/src/poo/MainCalculadora.java
@@ -4,56 +4,16 @@ import java.util.Scanner;
 
 public class MainCalculadora {
     public static void main(String[] args) {
-        int resultado=0;
-        int numero1;
-        int numero2;
+
+         Calculadora casio =new Calculadora();
 
 
-        Calculadora casio=new Calculadora();
 
-        Scanner lector =new Scanner(System.in);
-        System.out.println("Ingrese el primer numero");
-        numero1=lector.nextInt();
-        System.out.println("Ingrese el segundo numero");
-        numero2=lector.nextInt();
+        
 
-        casio.setNum1(numero1);
-        casio.setNum2(numero2);
 
-        int opcion;
-        System.out.println("""
-                1=suma
-                2=resta
-                3=multiplicacion
-                4=division
-                """);
-        System.out.println("Ingrese una opcion(1-4)");
-        opcion=lector.nextInt();
-        switch (opcion) {
-            case 1:
-                 resultado =casio.sumar();
 
-                break;
-            case 2:
-                 resultado =casio.restar();
-                break;
-            case 3:
-                resultado =casio.multiplicar();
-                break;
-            case 4:
-                resultado =casio.dividir();
-                break;
-            
-            default:{
-                System.out.println("Operacion incorrecta");
-            }
-                break;
+
+
         }
-
-System.out.println(resultado);
-       
-
-       
-
-}
 }
---
+++ Java/CursoJava/JavaFundamentos/src/poo/Validaciones.java
@@ -0,0 +1,12 @@
+package poo;
+
+public class Validaciones {
+    
+   
+
+public boolean validarDivision(int divisor){
+    return divisor !=0;
+
+}
+
+}
